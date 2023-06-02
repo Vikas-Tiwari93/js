8101cbb728edd72374894eae8a752102e7073e8a
vikas-tiwari93 <vikastiwarisync@gmail.com>
2023-06-02 21:56:05 +0530

    day

---
+++ daybyday/reversestring.js
@@ -0,0 +1,19 @@
+// arra.reverse()
+
+function reverse(input) {
+  let inputarr = input.split(' ');
+  return inputarr.reverse().join(' ');
+}
+
+console.log(reverse('i am a good boy'));
+// without inbuilt
+
+function reverse1(input) {
+  let inputarr = input.split(' ');
+  let arr = [];
+  for (let i = 0; i < inputarr.length; i++) {
+    arr.push(inputarr[inputarr.length - 1 - i]);
+  }
+  return arr.join(' ');
+}
+console.log(reverse1('i am a good boy'));
---
+++ romannumber.js
@@ -0,0 +1,4 @@
+function convertoroman(input) {
+  let roman = { 1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 100: 'C', 500: 'D', 1000: 'M' };
+}
+convertoroman(90);
