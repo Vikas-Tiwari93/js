2ed7834038c88a52bafa27496d4e21b78705b6f7
vikas-tiwari93 <vikastiwarisync@gmail.com>
2023-06-03 18:35:19 +0530

    dayily

--- daybyday/reversestring.js
+++ daybyday/reversestring.js
@@ -18,3 +18,41 @@ function reverse1(input) {
 }
 console.log(reverse1('i am a good boy'));
 // anagram
+
+function ana(input1, input2) {
+  if (input1.length === input2.length) {
+    const obj1 = {};
+    const obj2 = {};
+    let count = 1;
+    for (let i = 0; i < input1.length; i++) {
+      if (!obj1.hasOwnProperty(input1[i])) {
+        count = 1;
+        obj1[input1[i]] = count;
+      } else {
+        obj1[input1[i]] += 1;
+      }
+      if (!obj2.hasOwnProperty(input2[i])) {
+        count = 1;
+        obj2[input2[i]] = count;
+      } else {
+        obj2[input2[i]] += 1;
+      }
+    }
+    const keysobj1 = Object.keys(obj1);
+    let a = false;
+    for (let i = 0; i < keysobj1.length; i++) {
+      if (obj2[keysobj1[i]] === obj1[keysobj1[i]]) {
+        a = true;
+      } else {
+        a = false;
+        break;
+      }
+    }
+    if (a === true) {
+      return 'anagram';
+    }
+    return 'not an anagram';
+  }
+  return 'not an anagram';
+}
+console.log(ana('sdder', 'dsder'));
