d6606997f7a829da2ee9e021ea478036b367c4bf
vikas-tiwari93 <vikastiwarisync@gmail.com>
2023-06-04 19:59:35 +0530

    big no without sorting

--- daybyday/sorting.js
+++ daybyday/sorting.js
@@ -24,3 +24,39 @@ function sorting1(input) {
 }
 
 console.log(sorting1([1, 3, 5, 1, 7, 9, 3, 4, 5]));
+
+//prime
+function prime(number) {
+  for (let i = 2; i < (number + 1) / 2; i++) {
+    if (number % i === 0) {
+      console.log(i);
+      return 'not a prime';
+    }
+  }
+  return 'prime';
+}
+console.log(prime(53));
+
+// 2 bigest numbers
+
+function twobig(input) {
+  let arr = [];
+  let number;
+  for (let i = 0; i < input.length; i++) {
+    number = input[0];
+    if (input[i] > number) {
+      number = input[i];
+      input.splice(i, 1);
+    }
+  }
+  arr.push(number);
+  for (let i = 0; i < input.length; i++) {
+    number = input[0];
+    if (input[i] > number && input[i] !== arr[0]) {
+      number = input[i];
+    }
+  }
+  arr.push(number);
+  return arr;
+}
+console.log(twobig([1, 2, 6, 5, 4, 8, 7, 9]));
