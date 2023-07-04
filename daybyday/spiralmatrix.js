a972b20da99a4372ced9449356e59f3e6a630de9
vikas-tiwari93 <vikastiwarisync@gmail.com>
2023-07-04 11:03:15 +0530

    spiral

---
+++ daybyday/spiralmatrix.js
@@ -0,0 +1,8 @@
+// Spiral Matrix Printing | Print the elements of a matrix in spiral form
+// // Input
+// var input = [
+//   [1, 2, 3, 4],
+//   [5, 6, 7, 8],
+//   [9, 10, 11, 12],
+//   [13, 14, 15, 16],
+// ];
